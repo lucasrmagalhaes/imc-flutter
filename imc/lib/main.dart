48b94dccb6971face29b9002acb0daf5e09d6353
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2023-11-13 19:57:43 -0300

    Código formatado

--- imc/lib/main.dart
+++ imc/lib/main.dart
@@ -11,12 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'IMC',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomePage()
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'IMC',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const HomePage());
   }
 }
--- imc/lib/screens/home_page.dart
+++ imc/lib/screens/home_page.dart
@@ -18,47 +18,44 @@ class HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Calculadora de IMC')),
-      body: SingleChildScrollView(
-        child: Column(children: <Widget> [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 0.0),
-            child: Text('Insira as informações', style: TextStyle(fontSize: 25.0)),
-          ),
-          _campo('Peso (Kg)', _controllerPeso),
-          _campo('Altura (cm)', _controllerAltura),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 16.0),
-            child: Text(_resultado, style: const TextStyle(fontSize: 25.0))
-          ),
-          Container(
-            height: 50.0,
-            width: 200.0,
-            color: Colors.blue[800],
-            child: ElevatedButton(
-              onPressed: () {
-                double? peso = double.tryParse(_controllerPeso.text);
-                double? altura = double.tryParse(_controllerAltura.text);
-      
-                if (peso != null && altura != null) {
-                  altura /= 100.0;
+        appBar: AppBar(title: const Text('Calculadora de IMC')),
+        body: SingleChildScrollView(
+          child: Column(children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.fromLTRB(8.0, 16.0, 8.0, 0.0),
+              child: Text('Insira as informações',
+                  style: TextStyle(fontSize: 25.0)),
+            ),
+            _campo('Peso (Kg)', _controllerPeso),
+            _campo('Altura (cm)', _controllerAltura),
+            Padding(
+                padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 16.0),
+                child:
+                    Text(_resultado, style: const TextStyle(fontSize: 25.0))),
+            Container(
+                height: 50.0,
+                width: 200.0,
+                color: Colors.blue[800],
+                child: ElevatedButton(
+                  onPressed: () {
+                    double? peso = double.tryParse(_controllerPeso.text);
+                    double? altura = double.tryParse(_controllerAltura.text);
 
-                  double imc = (peso / (altura * altura));
-                  String imcFormatado = NumberFormat("0.00").format(imc);
+                    if (peso != null && altura != null) {
+                      altura /= 100.0;
 
-                  setState(() {
-                    _resultado = 'IMC: $imcFormatado';
-                  });
-                }
+                      double imc = (peso / (altura * altura));
+                      String imcFormatado = NumberFormat("0.00").format(imc);
 
-                
-              },
-              child: const Text('Calcular IMC'),
-            )
-          )
-        ]),
-      )
-    );
+                      setState(() {
+                        _resultado = 'IMC: $imcFormatado';
+                      });
+                    }
+                  },
+                  child: const Text('Calcular IMC'),
+                ))
+          ]),
+        ));
   }
 }
 
@@ -69,12 +66,9 @@ _campo(String texto, TextEditingController controller) {
       controller: controller,
       keyboardType: TextInputType.number,
       decoration: InputDecoration(
-        labelText: texto,
-        border: const OutlineInputBorder(),
-        labelStyle: TextStyle(
-          fontSize: 25.0, color: Colors.blue[800]
-        )
-      ),
+          labelText: texto,
+          border: const OutlineInputBorder(),
+          labelStyle: TextStyle(fontSize: 25.0, color: Colors.blue[800])),
     ),
   );
 }
